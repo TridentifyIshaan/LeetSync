779ae3ee4e1c30bd71c4717080b5b2746d3dfc29
Ishaan Rastogi <97788132+TridentifyIshaan@users.noreply.github.com>
2026-03-08 17:20:32 +0530

    Time: 2 ms (99.22%) | Memory: 47 MB (71.67%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -2,15 +2,13 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         // Create a hash map to store value and index
         Map<Integer, Integer> map = new HashMap<>();
+        // Consolidated the previous code into one loop (One Pass)
         for ( int i = 0; i < nums.length; i++ ) {
-            map.put(nums[i], i);
-        }
-        // Iterate through the array and check for complement
-        for ( int j = 0; j < nums.length; j++ ) {
-            int diff = target - nums[j]; // complement
-            if ( map.containsKey(diff) && map.get(diff) != j ) {
-                return new int[] { j, map.get(diff) };
+            int diff = target - nums[i]; // complement
+            if ( map.containsKey(diff) && map.get(diff) != i ) {
+                return new int[] { i, map.get(diff) };
             }
+            map.put(nums[i], i);
         }
         return null;
     }
